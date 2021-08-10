3dc20346187c4313a533d5d8152dac91a7f92bf0
maneesharandeniya <randeniyamaneesha13@gmail.com>
2021-08-11 02:42:19 +0530

    "OAuth failure handler added"

--- src/main/java/com/slcinema/security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/slcinema/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -37,34 +37,26 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
 
-        String targetUrl = determineTargetUrl(request, response, authentication);
-        System.out.println("uuuu");
+        String targetUrl = CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
+                .map(Cookie::getValue)
+                .orElse(("/"));
 
+       // System.out.println(redirectUri);
 
-        clearAuthenticationAttributes(request, response);
-        ///response.sendRedirect("/user");
-        getRedirectStrategy().sendRedirect(request, response, targetUrl);
-        System.out.println(targetUrl);
-        //this.setDefaultTargetUrl("/cinema/movie/all");
-    }
-
-    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-        Optional<String> redirectUri = CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
-                .map(Cookie::getValue);
-        System.out.println(redirectUri);
-
-        String targetUrl = redirectUri.orElse(getDefaultTargetUrl());
+       // String targetUrl = redirectUri.orElse(getDefaultTargetUrl());
         System.out.println(targetUrl);
 
         String token = tokenProvider.generateToken((UserDetails) authentication.getPrincipal());
         System.out.println(UriComponentsBuilder.fromUriString(getDefaultTargetUrl())
                 .queryParam("token", token)
                 .build().toUriString());
-        return UriComponentsBuilder.fromUriString(targetUrl)
-                .queryParam("token", token)
-                .build().toUriString();
-    }
 
+        clearAuthenticationAttributes(request, response);
+
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+        System.out.println(targetUrl);
+
+    }
 
 
     protected void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
